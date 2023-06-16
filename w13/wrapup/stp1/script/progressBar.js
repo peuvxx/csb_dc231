a86ae7acec71d4e32fe0e267eae07d8a35c8f5f7
peuvxx <126849534+peuvxx@users.noreply.github.com>
2023-06-16 11:32:31 +0900

    기말 과제

---
+++ w13/wrapup/stp1/script/progressBar.js
@@ -0,0 +1,25 @@
+const indicator =
+  document.querySelector(".indicator");
+const progressTarget =
+  document.querySelector("main");
+
+const progressBar = () => {
+  const progressTargetBCR =
+    progressTarget.getBoundingClientRect();
+  const progress =
+    1 -
+    progressTargetBCR.bottom /
+      progressTargetBCR.height;
+  console.log(progress);
+  if (progress >= 0 && progress <= 1) {
+    indicator.style.cssText =
+      "transform: scaleX(" + progress + ")";
+  } else if (progress < 0) {
+    indicator.style.cssText =
+      "transform: scaleX(0)";
+  }
+};
+
+document.onscroll = () => {
+  progressBar();
+};
